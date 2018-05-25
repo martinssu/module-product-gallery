936fd2168821cb78107bd59f53dcbdcd9ba0ec9e
IAmOmicron <rob.aimes95@gmail.com>
2018-05-25 23:17:11 +0100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Aimes_ProductGallery
+
+Magento 2 module: Replace the Fotorama gallery on product pages with a slick.js variant
---
+++ registration.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Copyright © Rob Aimes - https://aimes.eu
+ */
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Aimes_ProductGallery',
+    __DIR__
+);
